d58103fcd6f47df6fd5d34932cfeb0600f9e0d8a
MachadoAndre92 <45774991+MachadoAndre92@users.noreply.github.com>
2023-07-24 23:00:52 +0100

    [FIX] TEXT
    Texto da temperatura e humidade estavam a branco

--- resources/js/src/components/Leitura.jsx
+++ resources/js/src/components/Leitura.jsx
@@ -9,15 +9,16 @@ export default function Leitura(props) {
     <div className="rounded overflow-hidden shadow-lg grid">
         <div className="px-6 py-4">
             <div className="font-bold text-xl mb-2">{props.name}</div>
-            <span className="flex items-center text-lg font-medium text-gray-900 dark:text-white">
-                <span className="flex w-2.5 h-2.5 bg-blue-600 rounded-full mr-1.5 flex-shrink-0">
-                    </span>Temperatura: <a className='font-normal'> {props.temp}ºC</a></span>
-            
-
-                    <span className="flex items-center text-lg font-medium text-gray-900 dark:text-white">
-                        <span className="flex w-2.5 h-2.5 bg-teal-500 rounded-full mr-1.5 flex-shrink-0">
-                            </span>Humidade: <a className='font-normal'>{props.hum}%</a></span>
-            
+            <div className="flex items-center text-lg font-medium text-gray-900 dark:text-white">
+                <span className="flex w-2.5 h-2.5 bg-blue-600 rounded-full mr-1.5 flex-shrink-0"></span>
+                <span className="font-bold text-black">Temperatura:</span>
+                 <a className='font-normal text-black'>{props.temp}ºC</a>
+            </div>
+            <div className="flex items-center text-lg font-medium text-gray-900 dark:text-white">
+                <span className="flex w-2.5 h-2.5 bg-teal-500 rounded-full mr-1.5 flex-shrink-0"></span>
+                <span className="font-bold text-black">Humidity:</span>
+                <a className='font-normal text-black'>{props.hum}%</a>
+            </div>
         </div>
         <p className="text-gray-700 text-base font-bold px-6">Conduta de ar</p>
 
